643ff8fb4dc241f79110d387017112d0f81fda4b
Kenny Diaz <kend77@gmail.com>
2017-10-07 15:49:00 -0400

    completed exercises

--- models/index.js
+++ models/index.js
@@ -8,9 +8,29 @@ const Tea = db.define('tea', {
   title: Sequelize.STRING,
   description: Sequelize.TEXT,
   price: Sequelize.INTEGER,
-  category: Sequelize.ENUM('green', 'black', 'herbal')
-}, {
-  // add more functionality to our Tea model here!
+  category: Sequelize.ENUM('green', 'black', 'herbal'),
+  dollarPrice: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return `$${this.price.toString().slice(0, -2)}.${this.price.toString().slice(-2)}`
+    }
+  }
 })
+
+Tea.findByCategory = function(category) {
+  return Tea.findAll({where : {category : category}})
+}
+
+Tea.prototype.findSimilar = function() {
+  return Tea.findAll({where : {$and: {category: this.category, title: {$ne: this.title}}}});
+}
+
+Tea.addHook('beforeCreate', tea => {
+  tea.title = tea.title.split(' ').map(word => {
+    return word[0].toUpperCase() + word.slice(1);
+  }).join(' ');
+})
+
+
 
 module.exports = { db, Tea };
